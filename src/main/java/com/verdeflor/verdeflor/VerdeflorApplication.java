d6d70b2692aaa8330af51028d394724fa004d775
joao <joaovictorfarias1610@gmail.com>
2024-11-04 10:35:42 -0300

    initial commit

---
+++ src/main/java/com/verdeflor/verdeflor/VerdeflorApplication.java
@@ -0,0 +1,13 @@
+package com.verdeflor.verdeflor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdeflorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdeflorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verdeflor/verdeflor/VerdeflorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdeflor.verdeflor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdeflorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
